07daaa75edd47537a9ac8c6a1ca21f178dc9b7c9
Alex Shiels <alex@thresholdstate.com>
2005-09-23 13:19:52 +0000

    add db_column_exists()
    git-svn-id: http://textpattern.googlecode.com/svn/development/crockery@942 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/lib/mdb.php
+++ textpattern/lib/mdb.php
@@ -109,6 +109,23 @@ function db_table_list() {
 	return $rs;
 }
 
+function db_column_exists($tbl, $col, $res=0) {
+	global $mdb_res;
+	$res = ($res ? $res : $mdb_res);
+
+	if (MDB_TYPE == 'pg') {
+		$cols = @pg_meta_data($mdb_res, $tbl);
+		return (!empty($cols[$col]));
+	}
+	else {
+		$cols = array();
+		if ($rs = mysql_query('describe '.$tbl)) 
+			while ($row = mysql_fetch_assoc($rs))
+				$cols[$row['Field']] = $row;
+		return (!empty($cols[$col]));
+	}
+}
+
 function db_last_insert_id($table, $res=0) {
 	global $mdb_res;
 	$res = ($res ? $res : $mdb_res);
