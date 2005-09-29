9e2e220ff27f8727352675bd44689effa9b6ae74
Pedro Palazón <kusorbox@gmail.com>
2005-09-29 15:17:08 +0000

    do not break Alex stuff!
    git-svn-id: http://textpattern.googlecode.com/svn/development/crockery@981 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/publish/taghandlers.php
+++ textpattern/publish/taghandlers.php
@@ -194,7 +194,7 @@ $LastChangedRevision$
 		$Form = fetch_form($form);
 		
 		$qparts = array(
-			($category) ? "category='$category'" : '1',
+			($category) ? "category='$category'" : '1=1',
 			"order by",
 			$sort,
 			($limit) ? "limit $limit" : ''
@@ -365,7 +365,7 @@ $LastChangedRevision$
 			if (!empty($Category2)) array_push($cat_condition, "(Category1='$Category2')","(Category2='$Category2')");
 			$cat_condition = (count($cat_condition)) ? join(' or ',$cat_condition) : '';
 
-			$q = array("select *, id as thisid, unix_timestamp(Posted) as posted from `".PFX."textpattern` where Status=4",
+			$q = array("select *, id as thisid, unix_timestamp(Posted) as posted from ".PFX."textpattern where Status=4",
 				($cat_condition) ? "and (". $cat_condition. ")" :'',
 				"and Posted <= now() order by Posted desc limit 0,$limit");
 
@@ -402,7 +402,7 @@ $LastChangedRevision$
 		$thetable = ($type=='s') ? 'section' : 'category';
 		$out ='<select name="'.$type.'" onchange="submit(this.form)">'.n.
 		t.'<option value=""></option>'.n;
-		$q[] = "select name,title from `".PFX."txp_".$thetable."` where name != 'default'";
+		$q[] = "select name,title from ".PFX."txp_".$thetable." where name != 'default'";
 		$q[] = ($thetable=='category') ? "and type='article'" : '';
 		$q[] = "order by name";
 
@@ -1323,7 +1323,7 @@ $LastChangedRevision$
 		$article = safe_row(
 			"*,ID as thisid, unix_timestamp(Posted) as posted",
 			"textpattern",
-			"ID=$ID");
+			"ID='".doSlash($ID)."'");
 		
 		return permlinkurl($article);
 	}
@@ -1859,7 +1859,8 @@ $LastChangedRevision$
 			'format' => ''			
 		), $atts));
 		
-		if (is_numeric($decimals) and $decimals >= 0) {
+		if (empty($decimals) || $decimals < 0) $decimals = 2;
+		if (is_numeric($decimals)) {
 			$decimals = intval($decimals);			
 		} else {
 			$decimals = 2;
