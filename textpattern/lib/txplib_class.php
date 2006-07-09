48531d4fa1c03e222b14718e655b4d20a9af26a9
Alex Shiels <alex@thresholdstate.com>
2006-07-09 06:14:11 +0000

    add txplib_class
    git-svn-id: http://textpattern.googlecode.com/svn/development/crockery@1462 2fea6a4d-a838-0410-917b-93a53c48e9d2

---
+++ textpattern/lib/txplib_class.php
@@ -0,0 +1,91 @@
+<?php
+
+// Copyright 2006 Alex Shiels http://thresholdstate.com/
+
+
+/*
+function do_markup($type, $in) {
+	static $objs;
+
+	// default to nl2br if the requested class is unavailable
+	if (@strtolower(get_parent_class($type)) != 'txpmarkup')
+		$type = 'txpNL2br';
+	$type = strtolower($type);
+
+	// Cache singleton objects
+	if (empty($objs[$type]))
+		$objs[$type] = new $type();
+
+	return $objs[$type]->doMarkup($in);
+}
+*/
+
+function is_subclass($child, $parent) {
+	// return true if class $child is the same as, or a child class of, $parent
+
+	if (strcasecmp($child, $parent) == 0)
+		return true;
+	$p = get_parent_class($child);
+	while ($p) {
+		if (strcasecmp($p, $parent) == 0)
+			return true;
+		$p = get_parent_class($p);
+	}
+	return false;
+}
+
+function get_ancestor($class) {
+	$p = get_parent_class($class);
+	while ($p) {
+		$class = $p;
+		$p = get_parent_class($class);
+	}
+	return strtolower($class);
+}
+
+function get_classes($parent) {
+	static $classes = array();
+
+	$parent = strtolower($parent);
+
+	if (!empty($classes[$parent]))
+		return $classes[$parent];
+
+	$classes[$parent] = array();
+	foreach (get_declared_classes() as $class) {
+		$class = strtolower($class);
+		$p = get_ancestor($class);
+		$classes[$p][$class] = $class;
+	}
+
+	return $classes[$parent];
+}
+
+function &get_singleton($class) {
+	static $objs = array();
+
+	$class = strtolower($class);
+	if (!empty($objs[$class]))
+		return $objs[$class];
+
+	$objs[$class] = new $class();
+	return $objs[$class];
+}
+
+function find_class_method($parent, $method) {
+	// find the first class from $parent or its children that has a method named $method
+	static $methods = array();
+
+	$method = strtolower($method);
+	if (!empty($methods[$parent][$method]))
+		return $methods[$parent][$method];
+
+	$classes = get_classes($parent);
+	foreach ($classes as $class)
+		if (is_callable(array($class, $method)))
+			return $methods[$parent][$method] = $class;
+
+	return '';
+}
+
+?>
