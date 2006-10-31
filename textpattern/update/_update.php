4473c3ce1c43dd2625dec798e1e712c109868b20
Alex Shiels <alex@thresholdstate.com>
2006-10-31 06:33:30 +0000

    fix _update code
    git-svn-id: http://textpattern.googlecode.com/svn/development/crockery@2056 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/update/_update.php
+++ textpattern/update/_update.php
@@ -25,13 +25,15 @@ $LastChangedRevision$
 	@set_time_limit(0);
 
 	// Run any update file newer than the last dbupdatetime
+	$newest = 0;
 	if ($txp_using_svn) {
 		$updated = false;
 		foreach (get_update_files() as $file) {
 			$f = txpath."/update/$file";
 			if (filemtime($f) > $dbupdatetime) {
 				include($f);
-				$updated = true;		
+				$updated = true;
+				$newest = max($newest, filemtime($f));
 			}
 		}
 		if (!$updated) return;
@@ -56,6 +58,7 @@ $LastChangedRevision$
 				if (version_compare($file_ver, $dbversion) > 0) {
 					include($f);
 					$dbversion = $file_ver;
+					$newest = max($newest, filemtime($f));
 				}
 			}
 		}
@@ -63,7 +66,7 @@ $LastChangedRevision$
 
 	// keep track of updates for svn users
 	safe_delete('txp_prefs',"name = 'dbupdatetime'");
-	safe_insert('txp_prefs', "prefs_id=1, name='dbupdatetime',val='".max(newest_file(),time())."', type='2'");
+	safe_insert('txp_prefs', "prefs_id=1, name='dbupdatetime',val='".max($newest,time())."', type='2'");
 	// update version
 	safe_delete('txp_prefs',"name = 'version'");
 	safe_insert('txp_prefs', "prefs_id=1, name='version',val='$dbversion', type='2'");
