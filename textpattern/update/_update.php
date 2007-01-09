70b3f92e0def2088afba74b8492891d0a2688ff9
Alex Shiels <alex@thresholdstate.com>
2007-01-09 21:26:04 +0000

    simplify _update.php
    git-svn-id: http://textpattern.googlecode.com/svn/development/crockery@2207 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/update/_update.php
+++ textpattern/update/_update.php
@@ -7,18 +7,20 @@ $LastChangedRevision$
 		exit("Nothing here. You can't access this file directly.");
 	global $txpcfg, $thisversion, $dbversion, $txp_using_svn, $dbupdatetime;
 
-	function get_update_files() {
+	function get_update_numbers() {
+		// get the numbers of all the _to_x.y.z.php files in the update directory
 		$files = array();
 		$dp = opendir(txpath.'/update/');
-		while (false !== ($file = readdir($dp))) 
+		$n = array();
+		while (false !== ($file = readdir($dp)))
 		{
-			if (strpos($file,"_to_") === 0)
-				$files[] = $file;
+			if (preg_match('/^_to_([0-9.]+)[.]php$/', $file, $m))
+				$n[] = $m[1];
 		}
 		closedir($dp);
 		// sort so they're in the correct order to apply
-		sort($files);
-		return $files;
+		sort($n);
+		return $n;
 	}
 
 	@ignore_user_abort(1);
@@ -26,26 +28,30 @@ $LastChangedRevision$
 
 	// Run any update file newer than the last dbupdatetime
 	$newest = 0;
-	if ($txp_using_svn) {
-		$updated = false;
-		foreach (get_update_files() as $file) {
-			$f = txpath."/update/$file";
-			if (filemtime($f) > $dbupdatetime) {
-				include($f);
-				$updated = true;
-				$newest = max($newest, filemtime($f));
-			}
+	$newversion = 0;
+
+	$updated = false;
+	foreach (get_update_numbers() as $n) {
+		$newversion = $n;
+		$f = txpath."/update/_to_{$n}.php";
+		if (filemtime($f) > $dbupdatetime) {
+			include($f);
+			$updated = true;
+			$newest = max($newest, filemtime($f));
 		}
-		if (!$updated) return;
 	}
+	if (!$updated) return;
+
+/*
+	FIXME: is any of this relevant?
 	else {
 		//Use "ENGINE" if version of MySQL > (4.0.18 or 4.1.2)
 		// On 4.1 or greater use utf8-tables, if that is configures in config.php
 		$mysqlversion = mysql_get_server_info();
-		$tabletype = ( intval($mysqlversion[0]) >= 5 || preg_match('#^4\.(0\.[2-9]|(1[89]))|(1\.[2-9])#',$mysqlversion)) 
+		$tabletype = ( intval($mysqlversion[0]) >= 5 || preg_match('#^4\.(0\.[2-9]|(1[89]))|(1\.[2-9])#',$mysqlversion))
 						? " ENGINE=MyISAM "
 						: " TYPE=MyISAM ";
-		if ( isset($txpcfg['dbcharset']) && (intval($mysqlversion[0]) >= 5 || preg_match('#^4\.[1-9]#',$mysqlversion))) 
+		if ( isset($txpcfg['dbcharset']) && (intval($mysqlversion[0]) >= 5 || preg_match('#^4\.[1-9]#',$mysqlversion)))
 		{
 			$tabletype .= " CHARACTER SET = ". $txpcfg['dbcharset'] ." ";
 		}
@@ -65,17 +71,18 @@ $LastChangedRevision$
 	}
 
 	if ( $dbversion !== '4.0.5' )
-	{  
+	{
 		if ((include txpath.DS.'update'.DS.'_to_4.0.5.php') !== false)
 			$dbversion = '4.0.5';
 	}
+*/
 
 	// keep track of updates for svn users
 	safe_delete('txp_prefs',"name = 'dbupdatetime'");
 	safe_insert('txp_prefs', "prefs_id=1, name='dbupdatetime',val='".max($newest,time())."', type='2'");
 	// update version
 	safe_delete('txp_prefs',"name = 'version'");
-	safe_insert('txp_prefs', "prefs_id=1, name='version',val='$dbversion', type='2'");
+	safe_insert('txp_prefs', "prefs_id=1, name='version',val='$newversion', type='2'");
 	// updated, baby. So let's get the fresh prefs and send them to languages
 	$event = 'prefs';
 	$step = 'list_languages';
