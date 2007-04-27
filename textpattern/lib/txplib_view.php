38dd8145b78705bd0ba07603186013ebcbeb3cbb
Alex Shiels <alex@thresholdstate.com>
2007-04-27 06:38:12 +0000

    add draft table view class
    git-svn-id: http://textpattern.googlecode.com/svn/development/crockery@2290 2fea6a4d-a838-0410-917b-93a53c48e9d2

---
+++ textpattern/lib/txplib_view.php
@@ -0,0 +1,86 @@
+<?php
+
+class TxpTableView {
+
+	var $class;
+	var $caption;
+	var $rows;
+	var $edit_actions = array();
+	var $count = 0;
+
+	function TxpTableView(&$rows, $caption='', $edit_actions=array()) {
+		$this->class = strtolower(get_class($this));
+		$this->caption = $caption;
+		$this->rows = $rows;
+		$this->edit_actions = $edit_actions;
+	}
+
+	function head($cols) {
+		$col = array();
+		$th = array();
+
+		foreach ($cols as $c) {
+			$col[] = '<col id="col-'.strtolower($c).'" />';
+			$th[] = tag(gTxt($this->class.'.col-'.strtolower($c)), 'th');
+		}
+
+		if ($this->edit_actions) {
+			$col[] = '<col id="col-multiedit" />';
+			$th[] = tag('&nbsp;', 'th');
+		}
+
+		return
+			'<colgroup>'.n.join(n, $col).n.'</colgroup>'.n.
+			'<thead>'.n.'<tr>'.n.join(n, $th).n.'</tr>'.n.'</thead>';
+
+	}
+
+	function body() {
+		$out = array();
+		foreach ($this->rows as $row) {
+			$out[] = $this->row($row);
+		}
+
+		return '<tbody>'.n.join(n, $out).n.'</tbody>';
+	}
+
+	function foot() {
+		return '';
+	}
+
+	function row($row) {
+		$tr = array();
+		foreach ($row as $v)
+			$tr[] = $v;
+
+		if ($this->edit_actions and isset($row['id']))
+			$tr[] = fInput('checkbox', 'selected[]', $row['id']);
+
+		return doWrap($tr, 'tr', 'td', 'row-'.(++$this->count % 2 ? 'odd' : 'even'));
+	}
+
+	function table() {
+		$out = array();
+
+		$out[] = $this->head(array_keys($this->rows[0]));
+
+		$out[] = $this->body();
+
+		$out[] = $this->foot();
+
+		return join(n, $out);
+	}
+
+	function render() {
+		$out = array();
+		$out[] = '<table class="txp-tableview '.$this->class.'">';
+		if ($this->caption)
+			$out[] = '<caption>'.escape_output($this->caption).'</caption>';
+		$out[] = $this->table();
+		$out[] = '</table>';
+		
+		return join(n, $out);
+	}
+}
+
+?>
