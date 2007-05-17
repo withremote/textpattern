0ef9d9c6792dfc4b3d6bae1c27deb6b8d9f3c097
Robert Wetzlmayr <r.wetzlmayr@gmail.com>
2007-05-17 07:24:44 +0000

    move image_data() from image-tab code into library to separate public functionality from element specific code
    git-svn-id: http://textpattern.googlecode.com/svn/development/crockery@2351 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/lib/txplib_image.php
+++ textpattern/lib/txplib_image.php
@@ -142,4 +142,97 @@ function check_gd($image_type) {
 	}
 }
 
+// -------------------------------------------------------------
+// Allow other - plugin - functions to
+// upload images without the need for writting duplicated code.
+
+function image_data($file , $meta = '', $id = '', $uploaded = true)
+{
+	global $txpcfg, $txp_user, $prefs, $file_max_upload_size;
+	$extensions = array(0,'.gif','.jpg','.png','.swf');
+
+	extract($txpcfg);
+
+	$name = $file['name'];
+	$error = $file['error'];
+	$file = $file['tmp_name'];
+
+	if ($uploaded) {
+		$file = get_uploaded_file($file);
+
+		if ($file_max_upload_size < filesize($file)) {
+			unlink($file);
+			return upload_get_errormsg(UPLOAD_ERR_FORM_SIZE);
+		}
+	}
+
+	list($w, $h, $extension) = @getimagesize($file);
+
+	if (($file !== false) && @$extensions[$extension]) {
+		$ext = $extensions[$extension];
+
+		$name = doSlash(substr($name, 0, strrpos($name, '.')).$ext);
+
+		if ($meta == false)	{
+			$meta = array('category' => '', 'caption' => '', 'alt' => '');
+		}
+
+		extract($meta);
+
+		$q = "
+			name = '$name',
+			ext = '$ext',
+			w = $w,
+			h = $h,
+			alt = '$alt',
+			caption = '$caption',
+			category = '$category',
+			date = now(),
+			author = '$txp_user'
+		";
+
+		if (empty($id)) {
+			$rs = safe_insert('txp_image', $q);
+			$id = $GLOBALS['ID'] = mysql_insert_id();
+		} else {
+			$id = assert_int($id);
+			$rs = safe_update('txp_image', $q, "id = $id");
+		}
+
+		if (!$rs) {
+			return gTxt('image_save_error');
+		} else {
+			$newpath = IMPATH.$id.$ext;
+
+			if (shift_uploaded_file($file, $newpath) == false) {
+				$id = assert_int($id);
+				safe_delete('txp_image', "id = $id");
+				safe_alter('txp_image', "auto_increment = $id");
+				if (isset($GLOBALS['ID'])) {
+					unset( $GLOBALS['ID']);
+				}
+
+				return $newpath.sp.gTxt('upload_dir_perms');
+			} else {
+				@chmod($newpath, 0644);
+
+				// Auto-generate a thumbnail using the last settings
+				if (isset($prefs['thumb_w'], $prefs['thumb_h'], $prefs['thumb_crop'])) {
+					img_makethumb($id, $prefs['thumb_w'], $prefs['thumb_h'], $prefs['thumb_crop']);
+				}
+
+				$message = gTxt('image_uploaded', array('{name}' => $name));
+				return array($message, $id);
+			}
+		}
+	} else { 
+		// missing or invalid file
+		if ($file === false) {
+			return upload_get_errormsg($error);
+		} else {
+			return gTxt('only_graphic_files_allowed');
+		}
+	}
+}
+
 ?>
