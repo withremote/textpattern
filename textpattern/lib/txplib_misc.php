d00c414093128abdb5d223bda6084709bf400295
Robert Wetzlmayr <r.wetzlmayr@gmail.com>
2007-05-27 10:14:31 +0000

    Merge [2394]
    git-svn-id: http://textpattern.googlecode.com/svn/development/crockery@2395 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/lib/txplib_misc.php
+++ textpattern/lib/txplib_misc.php
@@ -1428,7 +1428,7 @@ $LastChangedRevision$
 
 //-------------------------------------------------------------
 	function update_lastmod() {
-		safe_update("txp_prefs", "val = now()", "name = 'lastmod'");
+		safe_upsert("txp_prefs", "val = now()", "name = 'lastmod'");
 	}
 
 //-------------------------------------------------------------
