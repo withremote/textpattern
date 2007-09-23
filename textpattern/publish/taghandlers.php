6896623db2b93a298e935d330774e503863615af
Ruud van Melick <ruud@vanmelick.com>
2007-09-23 21:33:40 +0000

    crockery: merge [2656]
    git-svn-id: http://textpattern.googlecode.com/svn/development/crockery@2657 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/publish/taghandlers.php
+++ textpattern/publish/taghandlers.php
@@ -618,28 +618,25 @@ $LastChangedRevision$
 			'wraptag'  => '',
 		), $atts));
 
-		$pattern = '/\s*(parentid|name|discussid|web|email|ip|posted|message|visible)/';
-		$replace = 'd.\\1';
-		$sort = preg_replace($pattern, $replace, $sort);
+		$sort    = preg_replace('/\bposted\b/', 'd.posted', $sort);
 		$expired = ($prefs['publish_expired_articles']) ? '' : ' and (now() <= t.Expires or t.Expires = '.NULLDATETIME.') ';
 		
-		$fields = 'd.name, d.discussid, t.ID as thisid, unix_timestamp(t.Posted) as posted, unix_timestamp(t.Expires) as expires,'.
-			't.AuthorID, t.LastMod, t.LastModID, t.Title, t.Image, t.Category1, t.Category2, '.
-			't.Annotate, t.AnnotateInvite, t.comments_count, t.Status, t.Section, t.override_form, t.Keywords, t.url_title,'.
-			't.custom_1, t.custom_2, t.custom_3, t.custom_4, t.custom_5, t.custom_6, t.custom_7, t.custom_8, t.custom_9, t.custom_10, t.uid';
+		$rs = startRows('select d.name, d.discussid, t.ID as thisid, unix_timestamp(t.Posted) as posted, t.Title as title, t.Section as section, t.url_title '.
+				'from '. safe_pfx('txp_discuss') .' as d inner join '. safe_pfx('textpattern') .' as t on d.parentid = t.ID '.
+				'where t.Status >= 4'.$expired.' and d.visible = '.VISIBLE.' order by '.doSlash($sort).' limit 0,'.intval($limit));
 
-		$rs = startRows('select '. $fields .
-				' from '. safe_pfx('txp_discuss') .' as d inner join '. safe_pfx('textpattern') .' as t on d.parentid = t.ID '.
-				'where t.Status >= 4'.$expired.' and d.visible = '.VISIBLE.' order by '.doSlash($sort).' limit 0,'.intval($limit));	
-		if ($rs) {
-			while ($c = nextRow($rs)) {
+		if ($rs)
+		{
+			while ($c = nextRow($rs))
+			{
 				$out[] = href(
 					$c['name'].' ('.escape_title($c['Title']).')', 
 					permlinkurl($c).'#c'.$c['discussid']
 				);
 			}
 
-			if ($out) {
+			if ($out)
+			{
 				return doLabel($label, $labeltag).doWrap($out, $wraptag, $break, $class);
 			}
 		}
